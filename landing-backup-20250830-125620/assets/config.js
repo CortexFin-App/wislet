4c9da4a9f942769959ccad3f8b3e3f5904c6ee61
vvkukhar <vladkuhar22082007@gmail.com>
2025-09-14 18:20:39 +0300

    Delete landing-backup-20250830-125620 directory

--- landing-backup-20250830-125620/assets/config.js
+++
@@ -1,8 +0,0 @@
-
-// Fill these IDs before production deploy
-window.__CFIN_CONFIG = {
-  GA_MEASUREMENT_ID: "G-EXT1WL0VW1",
-  FB_PIXEL_ID: "123456789012345",
-  FORM_ENDPOINT: "", // e.g. Mailchimp/Formspree endpoint
-  YT_VIDEO_ID: ""    // for landing hero embed
-};
