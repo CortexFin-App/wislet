aaa8a38075c50d06bf3e22f121f35ba9d2444563
vvkukhar <vladkuhar22082007@gmail.com>
2025-09-14 18:20:57 +0300

    Delete landing-backup-20250830-125611 directory

--- landing-backup-20250830-125611/sw.js
+++
@@ -1,2 +0,0 @@
-﻿self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>{e.waitUntil((async()=>{try{const k=await caches.keys();await Promise.all(k.map(caches.delete));await self.registration.unregister()}finally{const cs=await self.clients.matchAll({type:"window"});for(const c of cs)c.navigate(c.url)}})())});
